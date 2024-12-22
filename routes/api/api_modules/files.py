c4eef93552d6eab750b4a5a1aeee75a8ef445f97
Y0rFa1se <neargul123@gmail.com>
2024-12-22 13:15:25 +0900

    tet

--- routes/api/api_modules/files.py
+++ routes/api/api_modules/files.py
@@ -10,7 +10,7 @@ async def upload(file, password):
     
     filename = file.filename
     
-    with open(os.path.join("../../files/API_SERVER", filename), "wb") as f:
+    with open(os.path.join("../storage/api_server_files/", filename), "wb") as f:
         while chunk := await file.read(1024 * 1024):
             f.write(chunk)
 
@@ -22,7 +22,7 @@ async def download(file_path, password):
     if pw != password:
         return {"status": "false"}
     
-    file_path = os.path.join("../../files/API_SERVER", file_path)
+    file_path = os.path.join("../storage/api_server_files/", file_path)
 
     def file_streamer():
         with open(file_path, "rb") as f:
@@ -30,18 +30,3 @@ async def download(file_path, password):
                 yield chunk
 
     return StreamingResponse(file_streamer(), media_type="application/octet-stream", filename=os.path.basename(file_path), headers={"Content-Disposition": "attachment; filename={file_path}"})
-
-async def download_stockdb(password):
-    pw = os.getenv("GUEST_PASSWORD")
-
-    if pw != password:
-        return {"status": "false"}
-    
-    file_path = "../../files/stock.db"
-
-    def file_streamer():
-        with open(file_path, "rb") as f:
-            while chunk := f.read(1024 * 1024):
-                yield chunk
-
-    return StreamingResponse(file_streamer(), media_type="application/octet-stream", headers={"Content-Disposition": "attachment; filename={file_path}"})
