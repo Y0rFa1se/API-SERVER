88f4ff07da7c50bcdb628225eb0895433dc9e8cd
Y0rFa1se <neargul123@gmail.com>
2024-12-22 12:49:51 +0900

    test

--- routes/api/config.py
+++ routes/api/config.py
@@ -4,8 +4,4 @@ router = APIRouter()
 
 @router.get("/ping")
 async def ping():
-    return {"response": "pong!!"}
-
-@router.get("/pong")
-async def pong():
-    return {"response": "pong!!"}
+    return {"response": "pong"}
